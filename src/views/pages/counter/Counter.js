2094521fceb0f1c2df6d639622bd956a0eb1873a
Thanawan <thanawan@qboxpoint.com>
2022-07-12 12:47:07 +0700

    Counter Page

--- src/views/pages/counter/Counter.js
+++ src/views/pages/counter/Counter.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Theme from '../../../assets/theme/Theme';
 
 function Counter() {
+  const [count, setCount] = useState(0);
+
+  const onClickMinus = () => {
+    setCount(count - 1);
+  };
+  const onClickReset = () => {
+    setCount(0);
+  };
+  const onClickAdd = () => {
+    setCount(count + 1);
+  };
+
   return (
     <Theme>
-      <div>Counter</div>
+      <div
+        className={
+          'd-flex justify-content-center' +
+          (count < 0 ? ' text-danger' : count > 0 ? ' text-success' : ' text-white-50')
+        }
+      >
+        <b style={{ fontSize: '15rem' }}>{count}</b>
+      </div>
+
+      <div className="d-flex justify-content-center align-items-center">
+        <button className="btn btn-light mx-3" style={{ width: '100px' }} onClick={onClickMinus}>
+          <b>MINUS</b>
+        </button>
+        <button className="btn btn-light mx-3" style={{ width: '100px' }} onClick={onClickReset} disabled={count === 0}>
+          <b>RESET</b>
+        </button>
+        <button className="btn btn-light mx-3" style={{ width: '100px' }} onClick={onClickAdd}>
+          <b>ADD</b>
+        </button>
+      </div>
     </Theme>
   );
 }
