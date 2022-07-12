7d5832d05aa5c176c2c5867b4887d446d7a2295d
Thanawan <thanawan@qboxpoint.com>
2022-07-12 13:20:22 +0700

    Revised method setState

--- src/views/pages/counter/Counter.js
+++ src/views/pages/counter/Counter.js
@@ -1,17 +1,19 @@
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
 import Theme from '../../../assets/theme/Theme';
 
 function Counter() {
   const [count, setCount] = useState(0);
 
-  const onClickMinus = () => {
-    setCount(count - 1);
+  const onClickDecrease = () => {
+    setCount((count) => count - 1);
   };
   const onClickReset = () => {
     setCount(0);
   };
-  const onClickAdd = () => {
-    setCount(count + 1);
+  const onClickIncrease = () => {
+    setCount((count) => count + 1);
   };
 
   return (
@@ -26,14 +28,18 @@ function Counter() {
       </div>
 
       <div className="d-flex justify-content-center align-items-center">
-        <button className="btn btn-light mx-3" style={{ width: '100px' }} onClick={onClickMinus}>
-          <b>MINUS</b>
+        <button className="btn btn-light mx-3" style={{ width: '100px' }} onClick={onClickDecrease}>
+          <b>
+            <FontAwesomeIcon icon={faMinus} />
+          </b>
         </button>
         <button className="btn btn-light mx-3" style={{ width: '100px' }} onClick={onClickReset} disabled={count === 0}>
           <b>RESET</b>
         </button>
-        <button className="btn btn-light mx-3" style={{ width: '100px' }} onClick={onClickAdd}>
-          <b>ADD</b>
+        <button className="btn btn-light mx-3" style={{ width: '100px' }} onClick={onClickIncrease}>
+          <b>
+            <FontAwesomeIcon icon={faPlus} />
+          </b>
         </button>
       </div>
     </Theme>
