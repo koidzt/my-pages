63402a757a6a8b863afba8f7a4435b73f1420145
Thanawan <thanawan@qboxpoint.com>
2022-09-22 17:22:07 +0700

    Add Sudoku game

--- src/views/pages/sudoku/Sudoku.js
+++ src/views/pages/sudoku/Sudoku.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import '../../../styles/views/pages/sudoku/sudoku.css';
 import Theme from '../../../assets/theme/Theme';
+import Board from './Board';
 
 export default class Sudoku extends Component {
   render() {
     return (
       <Theme>
-        <div>Sudoku</div>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+          <div style={{ width: 600, height: 500 }}>
+            <Board></Board>
+          </div>
+        </div>
       </Theme>
     );
   }
